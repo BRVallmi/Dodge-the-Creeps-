acbea8ba6ac8906d27ece84a4cb796918f509371
BRVallmi <118301519+BRVallmi@users.noreply.github.com>
2024-11-05 17:18:27 +0100

    Preparem les col·lisions

--- Player.gd
+++ Player.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+signal hit
 
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
 var screen_size # Mida de la finestra de joc.
@@ -34,3 +34,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
